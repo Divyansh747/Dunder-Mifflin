4cdfcd8afe14c370475f8cb92954e61183a799ed
divyansh747 <divyansh.1999.dr@gmail.com>
2021-02-16 02:25:13 +0530

    add remaining project directories

---
+++ backend/spring_boot_freelancing/src/main/java/com/divyansh/dunder/freelancing/SpringBootFreelancingApplication.java
@@ -0,0 +1,13 @@
+package com.divyansh.dunder.freelancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreelancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreelancingApplication.class, args);
+	}
+
+}
---
+++ backend/spring_boot_freelancing/src/test/java/com/divyansh/dunder/freelancing/SpringBootFreelancingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyansh.dunder.freelancing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFreelancingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
